d15ffcc4dd9cd7c3ffd23ef18db1ba040560b2c0
Vladyslav Kondratenko <vladyslav.kondratenko.dev@gmail.com>
2025-12-31 01:04:55 +0100

    Update `.gitignore`, bump `ESP_IDF_VERSION`, and add `Cargo.lock` file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use esp_idf_svc::log::EspLogger;
+use esp_idf_svc::sys;
+use log::info;
+
 fn main() {
-    // It is necessary to call this function once. Otherwise, some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+    // It is necessary to call this function once.
+    // Otherwise, some patches to the runtime,
+    // implemented by esp-idf-sys,
+    // might not link properly.
+    // See https://github.com/esp-rs/esp-idf-template/issues/71
+    sys::link_patches();
 
     // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+    EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    info!("Hello, world!");
 }

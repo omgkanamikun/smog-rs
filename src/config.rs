d418e6f46ab42aa81074e341489becfaad983251
Vlad Kondratenko <omgkanamikun@gmail.com>
2026-01-14 15:07:16 +0100

    Refactor CI workflow and improve network task handling (#6)
    * Reduce HTTP send interval and refactor network task for cleaner error handling and improved logging.
    
    * Refactor Rust CI workflow: split jobs for lints and release build, add permissions, caching, and install improvements.
    
    * Added TIMEZONE dummy env var for CI
    
    * Update Rust CI workflow: change default branch, add concurrency control, enhance ldproxy installation, and minor dependency adjustments
    
    ---------
    
    Co-authored-by: Vladyslav Kondratenko <vladyslav.kondratenko.dev@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
 pub(crate) const WIFI_SSID: &str = env!("WIFI_2GZ_SSID");
 pub(crate) const WIFI_PASS: &str = env!("WIFI_2GZ_PASS");
 pub(crate) const HTTP_SENDING_ENABLED: &str = env!("HTTP_SENDING_ENABLED");
-pub(crate) const HTTP_SEND_INTERVAL_MS: u64 = 60_000;
+pub(crate) const HTTP_SEND_INTERVAL_MS: u64 = 15_000;
 pub(crate) const HTTP_CONSUMER_ENDPOINT_URL: &str = env!("HTTP_CONSUMER_ENDPOINT_URL");
 pub(crate) const EXECUTION_DELAY_MS: u64 = 1000;
 pub(crate) const TIMESTAMP_PATTERN: &str = "%Y-%m-%d %H:%M:%S";
